01e1c9938fd928cd4c20a08e4cc1357ed99802ec
Heather Kirk <hskirk@gmail.com>
2019-03-27 03:24:59 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, SUM(pledges.amount) AS project_total FROM projects
 INNER JOIN pledges ON projects.id = pledges.project_id
 WHERE project_total >= 0
-GROUP BY project.title"
+GROUP BY projects.title"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
