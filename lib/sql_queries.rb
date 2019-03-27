cd21c6fa6bc07a2f82c02c165ed67bffa66583c5
Heather Kirk <hskirk@gmail.com>
2019-03-27 03:57:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ GROUP BY users.name, users.age"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, (SUM(pledges.amount - projects.funding_goal)) AS project_total FROM projects
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS project_total FROM projects
 JOIN pledges ON projects.id = pledges.project_id
 GROUP BY projects.title
 HAVING project_total >= 0"
